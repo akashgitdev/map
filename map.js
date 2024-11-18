8b8536955d9aa9f92c38bb9f20d9f9c94cabb4bb
akashgitdev <nagaakashduddupudi1712@email.com>
2024-11-18 16:43:59 +0530

    commit

---
+++ map.js
@@ -0,0 +1,11 @@
+// var arr=[2,3,4,5,6,7,8,9,10];
+// var arr1=arr.map(a=>a*a);//using arrow function
+// console.log(arr1);
+//    o/p:[
+//    4,  9, 16,  25, 36,
+//    49, 64, 81, 100 ]
+
+// var arr=["ram","siva","ganesh","shanmuk"];
+// var arr1=arr.map(a=>a.toUpperCase());//using arrow function
+// console.log(arr1)
+//    o/p:[ 'RAM', 'SIVA', 'GANESH', 'SHANMUK' ]
